6cf331a108b8488af9a2fcbc3491c076fab66286
codewithmsakhtar <s.akhtar.ril@gmail.com>
2026-06-16 09:48:46 +0530

    terrafor fies added

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "example203" {
+  name     = "example203"
+  location = "centralindia"
+}
---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+
+
+}
+
+
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+ 
+  features {}
+  subscription_id = " 772a80da-2bd7-43bd-8842-3b914f74c16a"
+  
+}
